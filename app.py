6b56a43cf926a6536cab055aedb0e619a2a9092b
William Matson <willedmats@Williams-MacBook-Pro.local>
2021-12-05 16:34:40 +0000

    Add flask app with POST and GET methods

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", defaults={'path': ''}, methods=["GET", "POST"])
+@app.route("/<path:path>", methods=["GET", "POST"])
+def entry_point(path):
+    ops = {
+        "add": add,
+        "subtract": sub,
+        "multiply": mult,
+        "divide": div
+    }
+    if request.method == "GET" and path == "":
+        return "Hello Ian, welcome to my HTTP server - I trust you know how to use it", 200, {'Content-Type': 'text/plain'}
+    try:
+
+        try: 
+            operation, num1, num2 = parse_vars(ops, path)
+        except Exception as e:
+            return str(e), 400
+
+        result = ops[operation](num1, num2)
+        if request.method == "GET":
+            return str(result), 200, {'Content-Type': 'text/plain'}
+        else:
+            return jsonify({"result": result}), 200, {'Content-Type': 'application/json'}
+            
+    except Exception as e:
+        return str(e), 500 #, {'Content-Type': 'text/plain'} if request.method == "GET" else {'Content-Type': 'application/json'}
+
+def parse_vars(ops, path):
+    if request.method == "GET":
+        vars = [var for var in path.split("/") if var]
+        if len(vars) > 0:
+            if vars[0] in ops:
+                if len(vars) == 3:
+                    if validate_number(vars[1]) and validate_number(vars[2]):
+                        return vars[0], float(vars[1]), float(vars[2])
+                    else:
+                        raise TypeError("Incorrect operand types, 2 NUMBERS must be supplied. Usage: /<operation>/<num1>/<num2>")
+                else:
+                    raise ValueError("Incorrect number of operands, 2 must be supplied. Usage: /<operation>/<num1>/<num2>")
+            else:
+                raise Exception("Invalid operation type, valid operations are add, subtract, multiply and divide. Usage: /<operation>/<num1>/<num2>")
+        else:
+            raise Exception("No arguments supplied to GET operation. Usage: /<operation>/<num1>/<num2>")
+    else:
+        json_data = request.get_json()
+        if json_data:
+            if "operation" in json_data and json_data["operation"] in ops:
+                operation = json_data["operation"]
+                if "arguments" in json_data and len(json_data["arguments"]) == 2:
+                    if validate_number(json_data["arguments"][0]) and validate_number(json_data["arguments"][1]):
+                        return operation, float(json_data["arguments"][0]), float(json_data["arguments"][1])
+                    else:
+                        raise TypeError('Incorrect argument types, 2 NUMBERS must be supplied. Usage: {"operation": "<operation>", "arguments": [ <num1>, <num2> ]}')
+                else:
+                    raise ValueError('Invalid arguments, 2 must be supplied. Usage: {"operation": "<operation>", "arguments": [ <num1>, <num2> ]}')
+            else:
+                raise Exception('Invalid operation type, valid operations are add, subtract, multiply and divide. Usage: {"operation": "<operation>", "arguments": [ <num1>, <num2> ]}')
+        else:
+            raise Exception('No json data received in POST request. Usage: {"operation": "<operation>", "arguments": [ <num1>, <num2> ]}')
+
+def validate_number(num):
+    try: 
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mult(num1, num2):
+    return num1 * num2
+
+
+def div(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        raise Exception("Division by 0")
